7c9baf1085d95ce753f2fa94472f0d7868b17d5c
Jacob Kattampilly <jacobkuriala@gmail.com>
2017-11-20 15:33:25 -0800

    added about page and main routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import  {ChartsModule} from "ng2-charts";
+import { Routes, RouterModule } from "@angular/router";
+import { HttpGdaxService } from './http-gdax.service';
+
 import { AppComponent } from './app.component';
 import { TestingGdaxwebSocketComponent } from './component/testing-gdaxweb-socket/testing-gdaxweb-socket.component';
 import { GdaxHttpComponent } from './component/gdax-http/gdax-http.component';
-import {HttpClientModule} from '@angular/common/http';
-import { HttpGdaxService } from './http-gdax.service';
 import { PriceChartComponent } from './component/price-chart/price-chart.component';
-import  {ChartsModule} from "ng2-charts";
 import { TopNavComponent } from './component/top-nav/top-nav.component';
+import { AboutComponent } from './component/about/about.component';
+
+
+
+const appRoutes: Routes = [
+  {path: '' ,component: PriceChartComponent},
+{path: 'about' ,component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
@@ -17,14 +27,16 @@ import { TopNavComponent } from './component/top-nav/top-nav.component';
     TestingGdaxwebSocketComponent,
     GdaxHttpComponent,
     PriceChartComponent,
-    TopNavComponent
+    TopNavComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     ChartsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
 
   ],
   providers: [HttpGdaxService],
